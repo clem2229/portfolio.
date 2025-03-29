b5dc30d8a3087637506c3cf05a7f66980e2dda3e
Clément <flores29clement@gmail.com>
2025-03-29 13:02:02 +0100

    message

---
+++ JS/inscription.js
@@ -0,0 +1,19 @@
+var card = document.querySelector('.card');
+card.addEventListener('click', function() {
+    card.classList.toggle('flip');
+});
+
+ var cards = document.querySelector('.cards');
+cards.addEventListener('click', function() {
+    cards.classList.toggle('flip');
+});
+
+ var cardss = document.querySelector('.cardss');
+cardss.addEventListener('click', function() {
+    cardss.classList.toggle('flip');
+});
+
+var cardsss = document.querySelector('.cardsss');
+cardsss.addEventListener('click', function() {
+    cardsss.classList.toggle('flip');
+ });
---
+++ JS/script.js
@@ -0,0 +1,4 @@
+function popup(){
+    alert("Rendez-vous a midi pour la prochaine émission ! ")
+    }
+
